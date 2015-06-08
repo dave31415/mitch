7a600f1fdbbf6d6df74e682eab782932fc2a8348
David Johnston <dave31415@gmail.com>
2015-06-08 19:57:02 -0400

    new way of calculating lift

--- mitch/lift.py
+++ mitch/lift.py
@@ -2,6 +2,10 @@ import numpy as np
 from readers import read_merged_messages
 from collections import defaultdict
 import date_utils as du
+from datetime import datetime, timedelta
+from readers import read
+from collections import Counter
+from matplotlib import pylab as plt
 
 
 def calculate_last_date(sorted_dates, this_date):
@@ -59,17 +63,102 @@ def make_last_date_lookup_function(messages=None):
     return last_date_function
 
 
-def count_sales_days_since_message(messages=None, purchase_dates=None):
+def count_sales_days_since_message(messages=None, purchases=None):
+    buffer_days = 60
+    delta_days = timedelta(buffer_days)
+
     if messages is None:
         print 'reading messages'
         messages = read_merged_messages()
 
-    if purchase_dates is None:
+    if purchases is None:
         print 'reading purchase dates'
-        purchase_dates = get_purchase_dates()
+        purchases = list(read('sales'))
 
-    message_dates = [du.mdy_to_date(i['']) for d in
+    message_dates = du.get_all_message_dates(messages)
+    message_date_min = min(message_dates)
+    #start at a date with enough time for the first messages to effect things
+    start_date = message_date_min + delta_days
+    end_date = max(message_dates)
 
+    n_dates = (end_date - start_date).days + 1
+    print 'start_date:', start_date
+    print 'end_date:', end_date
+    print 'n_dates: %s\n' % n_dates
+
+    purchase_dates_unique = du.get_all_purchase_dates(purchases)
+    purchase_dates_unique = [i for i in purchase_dates_unique if start_date <= i <= end_date]
+    n_purchase_dates_unique = len(purchase_dates_unique)
+
+    print 'purchase_start_date:', min(purchase_dates_unique)
+    print 'purchase_end_date:', max(purchase_dates_unique)
+    print 'n_purchase_dates_unique:', n_purchase_dates_unique
+
+    print 'making last date lookup function'
+
+    last_date_function = make_last_date_lookup_function(messages)
+
+    n_days_counter = Counter()
+    n_days_counter_random = Counter()
+
+    purchase_dates = [du.ymd_to_date(i['purchase_date']) for i in purchases]
+    n_purchase_dates = len(purchase_dates)
+
+    for purchase in purchases:
+        if 'purchase_date' not in purchase:
+            continue
+
+        purchase_date_string = purchase['purchase_date']
+        if not purchase_date_string:
+            continue
+
+        if 'customer_external_id' not in purchase:
+            continue
+
+        user_id = purchase['customer_external_id']
+
+        if not user_id:
+            continue
+
+        purchase_date = du.ymd_to_date(purchase_date_string)
+
+        last_message_date, n_days_since_last_message = \
+            last_date_function(user_id, purchase_date)
+
+        random_purchase_date_index = np.random.randint(n_purchase_dates)
+        random_purchase_date = purchase_dates[random_purchase_date_index]
+
+        last_message_date_random, n_days_since_last_message_random = \
+            last_date_function(user_id, random_purchase_date)
+
+        n_days_counter[n_days_since_last_message] += 1
+        n_days_counter_random[n_days_since_last_message_random] += 1
+
+    return n_days_counter, n_days_counter_random
+
+
+def plot_ratios(n_days_counter, n_days_counter_random,scale=1.0):
+    n_days = [n for n in n_days_counter.keys() if n < 100]
+
+    data = [(n, n_days_counter[n], n_days_counter_random[n],
+             (1.0 + n_days_counter[n])/(1.0 + n_days_counter_random[n]))
+            for n in n_days if n is not None]
+
+    days = np.array([d[0] for d in data])
+    ratio = np.array([d[3] for d in data])*scale
+
+    alpha = 0.35
+    beta = 10.0
+    baseline = 0.9
+
+    print days
+    model = baseline + alpha*np.exp(-days/beta)
+
+    plt.plot(days, ratio, color='blue')
+    plt.plot(days, baseline+days*0.0, color='gray', linestyle='--')
+    plt.plot(days, model, color='red')
+    plt.xlabel('N days since message')
+    plt.ylabel('Sales Lift')
 
 
 
